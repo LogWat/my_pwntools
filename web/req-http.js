134289fde64d2f8562b2d50f886d6936bb4262ca
LogWat <Nk8877wpt+Karan@gmail.com>
2021-08-23 23:21:52 +0900

    fetch

--- web/req-http.js
+++ web/req-http.js
@@ -1,12 +1,13 @@
-import fetch from 'node-fetch'
-
-fetch('http://localhost:5000/test', {
-    method: 'GET'
-})
-.then((Response) => {
-    if (Response.ok) {
-        console.log("[200 OK]", Response.text())
-    } else {
-        console.log("[ERROR]", Response.statusText)
-    }
-})
+// URL指定(method省略時GET)
+fetch('http://localhost:5000/test')
+    .then((response) => {
+        if (response.ok) {
+            return response.text()
+        } else {
+            throw new Error()
+        }
+    })
+    // レスポンスを正常にテキストに変換できた場合
+    .then(text => console.log(text))
+    // エラーだった場合
+    .catch(error => console.error(error))
